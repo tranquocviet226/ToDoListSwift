e4ddeadab7f8cf45c601b3fc5fcfed7ac52ea4e6
Tran Quoc Viet <viettq@vnext.com.vn>
2020-07-31 14:19:29 +0700

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by MacOS on 7/31/20.
+//  Copyright © 2020 Tran Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
